abf592554493124a954f51543264ada2548a0a7c
Pavel Trofimov <p.trofimov@gmail.com>
2024-02-08 15:22:39 +0300

    [INV] feat: access token fix

--- app.ts
+++ app.ts
@@ -13,13 +13,10 @@ import routes from './routes';
 const app = new Koa();
 const router = new Router({ prefix: '/api' });
 
-console.log(process.env.WEB_URL);
-
 app.use(helmet());
 app.use(cookie());
 app.use(cors({
   credentials: true,
-  exposeHeaders: ['Set-Cookie'],
   origin: ctx => {
     const requestOrigin = ctx.get('Origin');
 
--- resources/user/endpoints/refresh/handler.ts
+++ resources/user/endpoints/refresh/handler.ts
@@ -4,22 +4,12 @@ import { omit } from 'lodash';
 import logger from '../../../../logger';
 
 export default async (ctx: signUpContext) => {
-  console.log('object');
-
   const cookie = ctx.cookie;
 
   const refreshToken = cookie?.refreshToken;
-  console.log(ctx.cookies.get('refreshToken'));
-  console.log(ctx.request.headers);
-  console.log(cookie);
-
-  console.log(refreshToken);
 
   try {
     const { payload: refreshPayload, expired: refreshExpired } = verifyJWT(refreshToken);  
-
-    console.log(refreshExpired);
-    console.log(refreshPayload);
 
     if (refreshPayload && !refreshExpired) {
       const user = omit(refreshPayload, 'exp', 'iat');
